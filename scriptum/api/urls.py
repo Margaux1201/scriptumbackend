bb270442ad24c8c6c0580fc0bed944754ee03d3b
Margaux <margaux.courageux@gmail.com>
2025-08-07 13:18:30 +0200

    route DELETE, GET(en POST token) et PUT validée et fonctionnelle

--- scriptum/api/urls.py
+++ scriptum/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import UserCreateView, UserLoginView
+from .views import UserCreateView, UserLoginView, UserDeleteView, UserRetrieveView, UserUpdateView
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login')
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('delete/', UserDeleteView.as_view(), name='user-delete'),
+    path('getinfo/', UserRetrieveView.as_view(), name='user-getinfo'),
+    path('updateinfo/', UserUpdateView.as_view(), name='user-updateinfo')
 ]
--- scriptum/api/views.py
+++ scriptum/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import User
+from .utils import require_token
 from .serializers import UserSerializer, LoginSerializer
 
 # POST register/ pour inscrire un utilisateur
@@ -31,5 +32,34 @@ class UserLoginView(APIView):
             else:
                 return Response({'error': 'Mot de passe incorrect'}, status=status.HTTP_401_UNAUTHORIZED)
         
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#DELETE delete/ pour supprimer un compte utilisateur
+class UserDeleteView(APIView):
+    @require_token
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response({"message": "Compte supprimé"}, status=status.HTTP_204_NO_CONTENT)
+
+# POST getinfo/ pour récupérer toutes les données d'un utilisateur
+class UserRetrieveView(APIView):
+    @require_token
+    def post(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+# PUT updateinfo/ pour modifier les données d'un utilisateur
+class UserUpdateView(APIView):
+    @require_token
+    def put(self, request):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
