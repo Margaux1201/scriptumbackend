c37645ae06f357f160cb097ec821f2d80b7a1b5b
Margaux <margaux.courageux@gmail.com>
2025-08-27 20:08:45 +0200

    correction signe astro de model character

--- scriptum/api/models.py
+++ scriptum/api/models.py
@@ -278,29 +278,29 @@ class Character(models.Model):
         month = self.month_birth
 
         if (month == 1 and day >= 20) or (month == 2 and day <= 18):
-            return "Verseau"
+            return "♒ Verseau"
         elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
-            return "Poissons"
+            return "♓ Poissons"
         elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
-            return "Bélier"
+            return "♈ Bélier"
         elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
-            return "Taureau"
+            return "♉ Taureau"
         elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
-            return "Gémeaux"
+            return "♊ Gémeaux"
         elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
-            return "Cancer"
+            return "♋ Cancer"
         elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
-            return "Lion"
+            return "♌ Lion"
         elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
-            return "Vierge"
+            return "♍ Vierge"
         elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
-            return "Balance"
+            return "♎ Balance"
         elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
-            return "Scorpion"
+            return "♏ Scorpion"
         elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
-            return "Sagittaire"
+            return "♐ Sagittaire"
         elif (month == 12 and day >= 22) or (month == 1 and day <= 19):
-            return "Capricorne"
+            return "♑ Capricorne"
         else:
             return None
 
