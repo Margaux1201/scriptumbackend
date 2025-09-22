b9f85cecae7b80a76cf073852a6e9a5941ebca91
Margaux <margaux.courageux@gmail.com>
2025-09-22 19:14:23 +0200

    suppression de mkdire file dans settings (possible debug déploiement)

--- scriptum/scriptum/settings.py
+++ scriptum/scriptum/settings.py
@@ -161,12 +161,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# Créer les dossiers nécessaires
-if not os.path.exists(os.path.join(BASE_DIR, 'static')):
-    os.makedirs(os.path.join(BASE_DIR, 'static'))
-if not os.path.exists(os.path.join(BASE_DIR, 'staticfiles')):
-    os.makedirs(os.path.join(BASE_DIR, 'staticfiles'))
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
