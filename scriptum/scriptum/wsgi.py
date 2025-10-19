ab7306ad366a5bd1a6d6c39048d2ab4ca6c52121
Margaux <margaux.courageux@gmail.com>
2025-10-19 18:03:21 +0200

    debug du chemin api pour le deploy render

--- scriptum/scriptum/wsgi.py
+++ scriptum/scriptum/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Ajouter le dossier parent à sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scriptum.scriptum.settings')
 
