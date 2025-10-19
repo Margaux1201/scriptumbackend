ea2692c33ae6a42717abbb27183d2cbe752a30cd
Margaux <margaux.courageux@gmail.com>
2025-10-19 19:04:16 +0200

    DEFAULT_RENDERER_CLASSES debug fetch deploy render

--- scriptum/scriptum/settings.py
+++ scriptum/scriptum/settings.py
@@ -133,6 +133,9 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
 }
 
 
